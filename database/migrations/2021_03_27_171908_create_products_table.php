7e4af262b91b0afcf826266996757d9564fbaab4
Mike Mwangi <mikemwangi0551@gmail.com>
2021-04-03 00:20:07 +0300

    Homepage complete

--- database/migrations/2021_03_27_171908_create_products_table.php
+++ database/migrations/2021_03_27_171908_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string("name");
             $table->string("description");
-            $table->int("price");
+            $table->integer("price");
             $table->mediumText('product_image')->nullable();
             $table->timestamps();
         });
